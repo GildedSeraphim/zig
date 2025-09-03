81358d0bd752e1ddc69e585736abf169a9e622ab
GildedSeraphim <nathanishaaz@gmail.com>
2025-09-03 12:24:49 -0500

    fixed build system kinda sorta

--- build.zig
+++ build.zig
@@ -3,9 +3,10 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "example",
+        .name = "hello",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("example.zig"),
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.graph.host,
             .optimize = optimize,
         }),
     });
