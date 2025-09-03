a862d336e08e4827523ea49f24887d3f0176b571
GildedSeraphim <snathani@protonmail.com>
2025-09-03 12:17:11 -0500

    broken build

--- build.zig
+++ build.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "hello",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = b.standardOptimizeOption(.{}),
+        .name = "example",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("example.zig"),
+            .optimize = optimize,
+        }),
     });
+    b.default_step.dependOn(&exe.step);
 
     // ### Libraries ###
     exe.linkSystemLibrary("vulkan");
